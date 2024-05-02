574c4cc6be043c0179b1b46b0243745389d78d37
Cui Wenshuo <cuiwenshuo@gmail.com>
2024-05-02 08:56:27 +0800

    Initial commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    region = "ap-southeast-1"
+    key    = "terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = "ami-0f74c08b8b5effa56"
+  instance_type = "t2.nano"
+  tags = {
+    Name = "test_instance_111"
+  }
+}
